9c63e639b673800b91da208cda9bfd6ad39eb667
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-03-11 01:13:08 +0530

    new

--- src/server.js
+++ src/server.js
@@ -1,13 +1,7 @@
 const app = require('../src/app');
 require ('dotenv').config;
 
-app.use(
-    cors({
-      origin: "https://ai-code-reviewer-pi.vercel.app", 
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: true,
-    })
-  );
+
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 })
