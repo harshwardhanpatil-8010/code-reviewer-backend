c5ca05933c5db793d7515d125361d08ca0bb71d6
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-03-14 01:46:15 +0530

    new

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes');
 const cors = require('cors');
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 const app = express();
+connectDB();
 app.use(cors());
+dotenv.config();
+app.use(bodyParser.json());
 app.use(express.json());
+
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
 app.use('/ai',aiRoutes);
-
+app.use("/api/auth", authRoutes);
 module.exports = app;
